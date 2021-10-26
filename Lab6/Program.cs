e812264cdbf71dac3af6311c05a3b2528d7f3acf
Evgeniy Idt <evgeniy.k.bird@gmail.com>
2021-10-26 14:25:15 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите любое предложение.");
+            string s = Console.ReadLine();
+            string max="";
+            string[] slova = s.Split(' ');
+            for (int i = 1; i < slova.Length; i++)
+                if (slova[i].Length > max.Length)
+                    max = slova[i];
+             Console.WriteLine("Самое длинное слово: {0}", max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ z2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите любое предложение:");
+            string s = Console.ReadLine();
+            s = s.Replace(" ","");
+            s = s.ToLower();
+            char[] arr;
+            arr = s.ToArray();
+            Array.Reverse(arr);
+            string s1 = String.Concat<char>(arr);
+            if (s==s1)
+            {
+                Console.WriteLine("Предложение является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Предложение не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
